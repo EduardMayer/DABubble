6bce44152591d20974546d47536960ee19c3749f
Matthias Plank <matthias.plank@gmx.at>
2023-10-18 20:41:56 +0200

    main components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,28 @@ import { environment } from '../environments/environment';
 import { provideAuth,getAuth } from '@angular/fire/auth';
 import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StartscreenComponent } from './startscreen/startscreen.component';
+import { MainscreenComponent } from './mainscreen/mainscreen.component';
+import { LoginComponent } from './startscreen/login/login.component';
+import { RegisterComponent } from './startscreen/register/register.component';
+import { SidenavComponent } from './mainscreen/sidenav/sidenav.component';
+import { HeaderComponent } from './mainscreen/header/header.component';
+import { ChannelComponent } from './mainscreen/channel/channel.component';
+import { ThreadComponent } from './mainscreen/thread/thread.component';
+import { ChatComponent } from './mainscreen/chat/chat.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StartscreenComponent,
+    MainscreenComponent,
+    LoginComponent,
+    RegisterComponent,
+    SidenavComponent,
+    HeaderComponent,
+    ChannelComponent,
+    ThreadComponent,
+    ChatComponent
   ],
   imports: [
     BrowserModule,
