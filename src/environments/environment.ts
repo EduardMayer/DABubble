e10f8b612f0e743280dd4778a8fdcb831701a533
Matthias Plank <matthias.plank@gmx.at>
2023-10-18 19:40:25 +0200

    addFirebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'dabubble-aa8ad',
+    appId: '1:73912220171:web:0e604f1dd6f6f3f304e50e',
+    storageBucket: 'dabubble-aa8ad.appspot.com',
+    apiKey: 'AIzaSyD1r-PAOZNv13HuFqPGEjhc2l-oKzznAlI',
+    authDomain: 'dabubble-aa8ad.firebaseapp.com',
+    messagingSenderId: '73912220171',
+  },
+};
