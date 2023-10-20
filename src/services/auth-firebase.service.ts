163ee93ecb5b9ae58aa93810844a28e0da5365b1
Matthias Plank <matthias.plank@gmx.at>
2023-10-20 19:36:28 +0200

    authService createUser

--- src/services/auth-firebase.service.ts
+++ src/services/auth-firebase.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,19 @@ import { Injectable } from '@angular/core';
 export class AuthFirebaseService {
 
   constructor() { }
+
+  createUser(email:string , password:string){
+    const auth = getAuth();
+    createUserWithEmailAndPassword(auth, email , password)
+      .then((userCredential) => {
+        // Signed up 
+        const user = userCredential.user;
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // ..
+      });
+  }
 }
