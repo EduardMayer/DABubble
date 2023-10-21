6e8033aa5a58e9d3e1ad69a8a5513caa44f78b54
Matthias Plank <matthias.plank@gmx.at>
2023-10-21 21:10:26 +0200

    header

--- src/app/mainscreen/header/header.component.ts
+++ src/app/mainscreen/header/header.component.ts
@@ -1,15 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from 'src/models/user.class';
 import { AuthFirebaseService } from 'src/services/auth-firebase.service';
+import { UserFirebaseService } from 'src/services/user-firebase.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
 
+  user: User = new User({
+    birthdate: "2000-03-23", 
+    firstName: "Guest",
+    lastName: "User", 
+    id:"", 
+    mail:"", 
+    street:"", 
+    zipCode:"" 
+  }) ; 
 
-  constructor(private authService:AuthFirebaseService){}
+  constructor(private authService:AuthFirebaseService , private userService:UserFirebaseService){}
+
+  ngOnInit(): void {
+    //Get current UserID from Local Storage 
+    //Get User from userService wirth UserID
+  }
 
   logout(){
     this.authService.logout(); 
