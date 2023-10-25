83915c56f64af359131784f9ee8f29864eca6ed9
Eduard Mayer <119446765+EduardMayer@users.noreply.github.com>
2023-10-25 21:18:52 +0200

    Add icon for visible the Password and a new form

--- src/app/startscreen/register/register.component.ts
+++ src/app/startscreen/register/register.component.ts
@@ -9,10 +9,12 @@ import { FormControl, Validators, FormGroup } from '@angular/forms';
 export class RegisterComponent {
   constructor() {}
 
-  errorInfo: any = false;
   isNameInputActive = false;
   isEmailInputActive = false;
   isPasswordInputActive = false;
+  isPasswordVisible: boolean = false;
+  showPasswordImage: string = 'assets/img/icons/eye.png';
+  hidePasswordImage: string = 'assets/img/icons/hideeye.png';
   checkboxValue: boolean = false;
 
   contactForm = new FormGroup({
@@ -24,10 +26,26 @@ export class RegisterComponent {
     passwordInput: new FormControl('', [
       Validators.required,
       Validators.minLength(6),
+      this.passwordValidator,
     ]),
   });
 
+  passwordValidator(control: FormControl): { [key: string]: boolean } | null {
+    if (control.value) {
+      const hasUppercase = /[A-Z]/.test(control.value);
+      const hasSpecialCharacter = /[!@#$%^&*()_+]/.test(control.value);
+      if (!hasUppercase || !hasSpecialCharacter) {
+        return { invalidPassword: true };
+      }
+    }
+    return null;
+  }
+
   isButtonDisabled() {
     return this.contactForm.invalid || !this.checkboxValue;
+  }
+
+  togglePasswordVisibility() {
+    this.isPasswordVisible = !this.isPasswordVisible;
   }
 }
