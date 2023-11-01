8ecc855c834c9109d3443dd2e6a2d2e65eda11b2
FabianKWirth <135059007+FabianKWirth@users.noreply.github.com>
2023-11-01 10:42:02 +0100

    updated message-toolbar; implemented addReactionComponent

--- src/app/mainscreen/message/toolbar/toolbar.component.ts
+++ src/app/mainscreen/message/toolbar/toolbar.component.ts
@@ -9,17 +9,38 @@ import { Component, Input } from '@angular/core';
 export class ToolbarComponent {
   @Input() isOwnMessage: boolean | undefined;
   showMessageOptions: boolean = false;
+  showMessageReactions: boolean = false;
 
-  showOptions(event: Event) {
+  toggleOptions(event: Event) {
     event.stopPropagation();
-    this.showMessageOptions = true;
-    console.log("Show Options");
+    if (this.showMessageOptions) {
+      this.showMessageOptions = false;
+    } else {
+      this.showMessageOptions = true;
+      this.hideReactions(event);
+    }
   }
 
   hideOptions(event: Event) {
     event.stopPropagation();
     this.showMessageOptions = false;
-    console.log("Hide Options");
+
+  }
+
+  toggleReactions(event: Event) {
+    event.stopPropagation();
+    if (this.showMessageReactions) {
+      this.showMessageReactions = false;
+    } else {
+      console.log()
+      this.showMessageReactions = true;
+      this.hideOptions(event);
+    }
+  }
+
+  hideReactions(event: Event) {
+    event.stopPropagation();
+    this.showMessageReactions = false;
   }
 
   openMenu(event: Event) {
@@ -28,5 +49,11 @@ export class ToolbarComponent {
     // Additional logic to open the menu if needed
   }
 
- 
+  openReactions(event: Event) {
+    // Prevent event propagation to avoid triggering a mouseleave on the parent
+    event.stopPropagation();
+    // Additional logic to open the menu if needed
+  }
+
+
 }
