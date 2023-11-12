1a0b7214ad28844b413792d1627ffea9593575b1
Matthias Plank <matthias.plank@gmx.at>
2023-11-12 14:02:03 +0100

    addUserStatus to UserProfil

--- src/app/mainscreen/user-profil/user-profil.component.ts
+++ src/app/mainscreen/user-profil/user-profil.component.ts
@@ -1,22 +1,42 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { User } from 'src/models/user.class';
 import { UserProfilService } from 'src/services/user-profil.service';
+import { UserStatusFirebaseService } from 'src/services/user-status-firebase.service';
 
 @Component({
   selector: 'app-user-profil',
   templateUrl: './user-profil.component.html',
   styleUrls: ['./user-profil.component.scss']
 })
-export class UserProfilComponent {
+export class UserProfilComponent implements OnInit{
   
   @Input() user:User = new User(); 
 
-  constructor(private userProfilService: UserProfilService){
+  constructor(private userProfilService: UserProfilService, private userStatusService: UserStatusFirebaseService){
 
   }
 
+  ngOnInit(): void {
+    console.log("onInit");
+    this.getStatus();
+  }
+
+
+  
+
   close(){
     this.userProfilService.close(); 
+  }
+
+  getStatus(){
+    this.userStatusService.getUserStatus(this.user.id)
+      .then((result) => {
+        this.user.status = result;   
+      })
+      .catch((error) => {
+        console.log(error);    
+      }); 
+    
   }
 
 }
--- src/services/user-status-firebase.service.ts
+++ src/services/user-status-firebase.service.ts
@@ -1,5 +1,5 @@
-import { Injectable, inject } from '@angular/core';
-import { Database, onDisconnect, ref, set } from '@angular/fire/database';
+import { Injectable, OnInit, inject } from '@angular/core';
+import { Database, get, onDisconnect, ref, set } from '@angular/fire/database';
 import { Timestamp } from 'firebase/firestore';
 
 @Injectable({
@@ -11,21 +11,35 @@ export class UserStatusFirebaseService {
 
   constructor() { }
 
- 
+
 
   writeUserStatus(userId:string, status:string) {
     const userStatusDatabaseRef = ref(this.database, 'userStatus/' + userId);
-  
-    // Set the user's status
     set(userStatusDatabaseRef, {
       "status": status
     }).then(() => {
-      // Add onDisconnect to set status to 'offline' when the user disconnects
       onDisconnect(userStatusDatabaseRef).set({
         "status": "offline"
       });
     });
   }
+
+  getUserStatus(userId:string) {
+    const userStatusDatabaseRef = ref(this.database, 'userStatus/' + userId);
+    return get(userStatusDatabaseRef).then((snapshot) => {
+      if (snapshot.exists()) {
+        const status = snapshot.val().status;
+        console.log(status);
+        return status
+      } else {
+        return "Status not available"; // Or handle the absence of status as needed
+      }
+    }).catch((error) => {
+      console.error("Error getting user status:", error);
+      return "Error fetching status";
+    });
+  }
+
 
 
 }
