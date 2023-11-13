0eacfacacd91beb1666b439b6bac6d7a4576ca0c
Eduard Mayer <119446765+EduardMayer@users.noreply.github.com>
2023-11-13 13:39:35 +0100

    add docs to storage-firebase

--- src/services/storage-firebase.service.ts
+++ src/services/storage-firebase.service.ts
@@ -8,6 +8,13 @@ export class StorageFirebaseService {
   constructor(private storage: Storage) {}
   
 
+  /**
+ * Uploads an image file to Firebase Storage and returns the download URL.
+ * 
+ * @param {File} file - The image file to be uploaded.
+ * @returns {Promise<string>} - A promise that resolves to the download URL of the uploaded image.
+ * @throws {Error} - If the file is invalid or has an unsupported type.
+ */
   async uploadIMGFile(file: File): Promise<string> {
     if (!file || !file.type.startsWith('image/')) {
       throw new Error('Invalid file type');
@@ -28,21 +35,24 @@ export class StorageFirebaseService {
   }
 
 
- async uploadFile(file: File, folder: string): Promise<string> {
-    let storageRef: StorageReference;
+ /**
+ * Uploads a file to Firebase Storage in the specified folder and returns the download URL.
+ * 
+ * @param {File} file - The file to be uploaded.
+ * @param {string} folder - The folder in which the file will be stored.
+ * @returns {Promise<string>} - A promise that resolves to the download URL of the uploaded file.
+ * @throws {Error} - If the file is invalid or has an unsupported type.
+ */
+  async uploadFile(file: File, folder: string): Promise<string> {
     const timestamp = new Date().getTime();
-
-    if (file.type.startsWith('image/') || file.type === 'application/pdf') {
-      const fileName = `${file.name}_${timestamp}`;
-      storageRef = ref(this.storage, `${folder}/${fileName}`);
-      
-    } else {
+    if (!file.type.startsWith('image/') && file.type !== 'application/pdf') {
       throw new Error('Invalid file type');
     }
 
+    const fileName = `${file.name}_${timestamp}`;
+    const storageRef = ref(this.storage, `${folder}/${fileName}`);
     try {
-      await this.uploadFileToStorage(storageRef, file);
-      const url = await this.getDownloadUrl(storageRef);
+      const url = await this.uploadAndRetrieveUrl(storageRef, file);
       console.log('File URL:', url);
       return url;
     } catch (error) {
@@ -50,8 +60,27 @@ export class StorageFirebaseService {
       throw error;
     }
   }
+
+
+  /**
+ * Uploads a file to a specified Firebase Storage reference and returns the download URL.
+ * 
+ * @param {StorageReference} storageRef - The Firebase Storage reference where the file will be uploaded.
+ * @param {File} file - The file to be uploaded.
+ * @returns {Promise<string>} - A promise that resolves to the download URL of the uploaded file.
+ */
+  private async uploadAndRetrieveUrl(storageRef: StorageReference, file: File): Promise<string> {
+    await uploadBytes(storageRef, file);
+    return getDownloadURL(storageRef);
+  }
   
-  
+  /**
+ * Deletes an image from Firebase Storage based on the provided image path.
+ * 
+ * @param {string} imagePath - The path of the image in Firebase Storage.
+ * @returns {Promise<void>} - A promise that resolves when the image is successfully deleted.
+ * @throws {Error} - If there is an error during the deletion process.
+ */
   async deleteImage(imagePath: string): Promise<void> {
     try {
       const path = imagePath;
@@ -65,10 +94,23 @@ export class StorageFirebaseService {
   }
 
 
+  /**
+ * Uploads a file to a specified Firebase Storage reference.
+ * 
+ * @param {StorageReference} storageRef - The Firebase Storage reference where the file will be uploaded.
+ * @param {File} file - The file to be uploaded.
+ * @returns {Promise<void>} - A promise that resolves when the file is successfully uploaded.
+ */
   async uploadFileToStorage(storageRef: StorageReference, file: File): Promise<void> {
     await uploadBytes(storageRef, file);
   }
 
+  /**
+ * Retrieves the download URL for a file stored in Firebase Storage.
+ * 
+ * @param {StorageReference} storageRef - The Firebase Storage reference of the file.
+ * @returns {Promise<string>} - A promise that resolves to the download URL of the file.
+ */
   async getDownloadUrl(storageRef: StorageReference): Promise<string> {
     return getDownloadURL(storageRef);
   }
