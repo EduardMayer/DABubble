b191ea1077ed9a59bcd3616d4257d67eedeca2db
Sezgin Gün <sezgoku@gmail.com>
2023-11-15 20:06:52 +0100

    header add member to channel
    adding members to a channel

--- src/app/mainscreen/channel/channel.component.ts
+++ src/app/mainscreen/channel/channel.component.ts
@@ -1,10 +1,18 @@
-import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { Channel } from 'src/models/channel.class';
 import { Message } from 'src/models/message.class';
+import { User } from 'src/models/user.class';
 import { ChannelFirebaseService } from 'src/services/channel-firebase.service';
 import { IfChangedService } from 'src/services/if-changed-service.service';
 import { UserFirebaseService } from 'src/services/user-firebase.service';
+import { MatMenuTrigger } from '@angular/material/menu';
 
+import { NgModule } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { BrowserModule } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-channel',
@@ -13,13 +21,24 @@ import { UserFirebaseService } from 'src/services/user-firebase.service';
   providers: [IfChangedService] // Provide the service at the component level
 })
 export class ChannelComponent {
+  @ViewChild('menuTrigger') menuTrigger?: MatMenuTrigger;
+  @ViewChild('addMemberTrigger') addMemberTrigger?: MatMenuTrigger;
+
   messageTime: string = "";
   messagePath: string = "";
+  memberName: string = "";
+  newAddedToChannelUser = new User();
 
   constructor(
     public channelFirebaseService: ChannelFirebaseService,
     public userFirebaseService: UserFirebaseService
   ) {
+    if (channelFirebaseService.selectedChannel) {
+      channelFirebaseService.loadChannelMessages(channelFirebaseService.selectedChannel.id);// to be changed to currentChannel
+      this.messagePath = `channels/${channelFirebaseService.selectedChannel.id}/messages/`;
+    }
+    this.loadallChannelusers();
+
   }
 
   getMessageTime(message: Message) {
@@ -43,29 +62,176 @@ export class ChannelComponent {
 
   }
 
-  userOnCurrentChannel: any = [];
+  userOnCurrentChannel: User[] = [];
+
+
 
   loadallChannelusers() {
     this.userFirebaseService.loadedUsers.forEach(user => {
 
-      user.channels.forEach(channel => {
-        debugger;
+      const selectedChannelName = this.channelFirebaseService.selectedChannel?.channelName;
 
-        console.log('channel of the user is', channel);
-        console.log('now selected channel is', this.channelFirebaseService.selectedChannel?.channelName);
+      if (selectedChannelName) {
+        user.channels.forEach((channel, index) => {
+          if (user.channels.includes(selectedChannelName)) {
+            if (!this.userOnCurrentChannel.some((u: { fullName: string }) => u.fullName === user.fullName)) {
+              this.userOnCurrentChannel.push(user);
+            }
+          }
 
-
-        if (channel === this.channelFirebaseService.selectedChannel?.channelName) {
-          this.userOnCurrentChannel.push(user);
-        }
-
-
-      });
-
+          console.log('channel of the user is', channel);
+          console.log('now selected channel is', this.channelFirebaseService.selectedChannel?.channelName);
+        });
+      }
       console.log(user);
     });
-    console.log('all users fir in chasnnechannel', this.userOnCurrentChannel);
+    console.log('all users for in chasnnechannel', this.userOnCurrentChannel);
 
+  }
+
+  searchText: string = "";
+  searchResults: string[] = [];
+  searchResultsUsers: User[] = [];
+  searchResultsChannels: Channel[] = [];
+
+  testData: string[] = ["hallo", "Test", "Search"];
+
+
+  ngOnInit(): void {
+    //this.searchResults = ["Hallo", "Test", "Search"]; 
+    //this.userService.load(); 
+  }
+
+  sendData(event: Event) {
+
+    console.log(this.searchText);
+
+    if (this.searchText != "") {
+      //console.log(this.searchText);
+      //this.searchResults = this.testData.filter(s => s.includes(this.searchText)); 
+      this.getUsers();
+      this.getChannels();
+    }
+    else {
+      this.searchResults = [];
+      this.searchResultsUsers = [];
+      this.searchResultsChannels = [];
+    }
+  }
+
+  async getUsers() {
+
+    this.searchResultsUsers = [];
+    console.log("Loaded Users:");
+    console.log(this.userFirebaseService.loadedUsers);
+
+    this.userFirebaseService.loadedUsers.forEach(user => {
+      if (user.fullName.toUpperCase().includes(this.searchText.toUpperCase())) {
+        this.searchResultsUsers.push(user);
+      }
+    });
+
+
+    /*
+    this.userService.getUserForSearch(this.searchText)
+        .then( (users) => {
+          this.searchResultsUsers = users; 
+        });
+    */
+  }
+  getChannels() {
+
+
+    this.searchResultsChannels = [];
+    console.log("Loaded Channels:");
+    console.log(this.channelFirebaseService.loadedChannels);
+
+    this.channelFirebaseService.loadedChannels.forEach(channel => {
+      if (channel.channelName.toUpperCase().includes(this.searchText.toUpperCase())) {
+        this.searchResultsChannels.push(channel);
+      }
+    });
+
+    /*
+    this.userService.getChannelForSearch(this.searchText)
+    .then( (channels) => {
+      this.searchResultsChannels = channels; 
+    });
+    */
+  }
+
+
+  selectUserfromSearchings(resultUsers: any) {
+    console.log(this.channelFirebaseService.selectedChannel);
+    console.log("Send New Message to User with ID: ");
+    console.log(this.searchResultsUsers[resultUsers]);
+
+
+
+    if (this.channelFirebaseService.selectedChannel?.channelName) {
+
+      this.newAddedToChannelUser = new User(resultUsers);
+
+      this.newAddedToChannelUser.channels.push(this.channelFirebaseService.selectedChannel.channelName);
+      this.searchText = '';
+
+
+    }
+
+  }
+
+  addUserToChannel(event: Event) {
+    event.stopPropagation();
+debugger;
+    if(this.newAddedToChannelUser.fullName) {
+      let newAddedToChannelUserToJson = this.newAddedToChannelUser.toJSON();
+
+      this.userFirebaseService.update(this.newAddedToChannelUser);
+      this.loadallChannelusers();
+      this.newAddedToChannelUser.fullName = '';
+      this.closeMenus();
+    }
+   
+
+  }
+
+  closeMenus() {
+    if (this.menuTrigger) {
+      this.menuTrigger.closeMenu();
+    }
+    if (this.addMemberTrigger) {
+      this.addMemberTrigger.closeMenu();
+    }
+  }
+
+  closeMenuViewUsersOnChannel() {
+    if (this.menuTrigger) {
+      this.menuTrigger.closeMenu();
+    }
+
+  }
+
+  shouldOpenAddMemberMenu = false;
+
+
+  openAddMemberMenu() {
+    // Verzögerung hinzufügen
+    if (this.menuTrigger) {
+      this.menuTrigger.closeMenu();
+    }
+      if (this.addMemberTrigger) {
+        this.addMemberTrigger.openMenu();
+      }
+  
+  }
+
+  cancelSelection() {
+      this.newAddedToChannelUser.fullName = '';
+
+  }
+
+  clickChannel(index: any) {
+    this.channelFirebaseService.selectChannel(this.searchResultsChannels[index].id);
   }
 
 }
