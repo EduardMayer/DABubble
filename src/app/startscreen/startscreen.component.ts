9176b86a182240e369bfacdb97b2786e02acb6ab
FabianKWirth <135059007+FabianKWirth@users.noreply.github.com>
2023-12-19 23:36:11 +0100

    updated Documentation

--- src/app/startscreen/startscreen.component.ts
+++ src/app/startscreen/startscreen.component.ts
@@ -26,11 +26,11 @@ export class StartscreenComponent implements OnInit{
 
 
   /**
- * Angular lifecycle hook called after component initialization.
- * Redirects to the 'main' route if the user is already logged in and the login session is not expired.
- * 
- * @returns {void}
- */
+  * Angular lifecycle hook called after component initialization.
+  * Redirects to the 'main' route if the user is already logged in and the login session is not expired.
+  * 
+  * @returns {void}
+  */
   ngOnInit(): void {
     document.body.style.overflow = 'hidden';
     if(this.authService.isLoggedIn() && !this.authService.loginExprired()){
@@ -38,11 +38,12 @@ export class StartscreenComponent implements OnInit{
     }
   }
 
+
   /**
- * Closes the imprint and privacy policy views by setting their respective flags to false.
- * 
- * @returns {void}
- */
+  * Closes the imprint and privacy policy views by setting their respective flags to false.
+  * 
+  * @returns {void}
+  */
   closeImprintAndPrivacy(){
     this.imprint = false; 
     this.privacyPolicy = false; 
@@ -50,53 +51,62 @@ export class StartscreenComponent implements OnInit{
 
 
   /**
- * Closes the registration view and shows the avatar view.
- * 
- * @returns {void}
- */
+  * Closes the registration view and shows the avatar view.
+  * 
+  * @returns {void}
+  */
   closeRegister(){
     this.showRegister = false; 
     this.showAvatar = true; 
   }
 
+
   /**
- * Closes the avatar view and shows the login view.
- * 
- * @returns {void}
- */
+  * Closes the avatar view and shows the login view.
+  * 
+  * @returns {void}
+  */
   closeAvatar(){
     this.showLogin = true;
     this.showAvatar = false;
   }
 
+
   /**
- * Closes the forgot password view and shows the login view.
- * 
- * @returns {void}
- */
+  * Closes the forgot password view and shows the login view.
+  * 
+  * @returns {void}
+  */
   closeForgotPassword(){
     this.showLogin = true;
     this.showForgotPassword = false;
   }
 
+
   /**
- * Shows the forgot password view and hides the login view.
- * 
- * @returns {void}
- */
+  * Shows the forgot password view and hides the login view.
+  * 
+  * @returns {void}
+  */
   onForgotPasswordLinkClick() {
     this.showLogin = false;
     this.showForgotPassword = true;
   }
 
 
+  /**
+  * Handles the completion of the introduction.
+  */
   handleIntroComplete(introComplete: boolean){
     document.body.style.overflow = 'auto';
     this.introComplete=introComplete;
   }
 
+
+  /**
+  * Opens the imprint and sets the privacy policy flag to true.
+  */
   handleOpenImprint(){
-    this.handleIntroComplete(true);
     this.privacyPolicy=true;
   }
 }
