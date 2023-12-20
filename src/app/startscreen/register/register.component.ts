7433c405e0a25fc17a89e774ecc1d2cc40fc302f
FabianKWirth <135059007+FabianKWirth@users.noreply.github.com>
2023-12-20 12:01:26 +0100

    Pw valid text pdate & bugfix message edit in private chat

--- src/app/startscreen/register/register.component.ts
+++ src/app/startscreen/register/register.component.ts
@@ -41,7 +41,7 @@ export class RegisterComponent {
     ]),
     passwordInput: new FormControl('', [
       Validators.required,
-      Validators.minLength(6),
+      Validators.minLength(8),
       Validators.pattern(/^(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*()_+]).*$/)
     ])
   });
